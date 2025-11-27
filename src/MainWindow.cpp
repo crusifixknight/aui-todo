611661286ffd9fed404ab1789a206ea3f00f3f41
crusifixknight <equinox.salieri@gmail.com>
2025-11-27 21:26:45 +0300

    remove trash

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -5,43 +5,17 @@
 #include <AUI/Platform/APlatform.h>
 #include <AUI/View/ADrawableView.h>
 #include <AUI/View/AProgressBar.h>
+#include <AUI/Platform/AWindow.h>
 
 using namespace declarative;
 
-MainWindow::MainWindow(_<MyUpdater> updater)
-  : AWindow("Project template app", 300_dp, 200_dp), mUpdater(std::move(updater)) {
-    setContents(Centered { Vertical {
-      Centered { Icon { ":img/icon.svg" } AUI_WITH_STYLE { FixedSize(64_dp) } },
-      Centered { Label { "Hello world from AUI!" } },
-      _new<AButton>("Visit GitHub repo")
-          .connect(&AView::clicked, this, [] { APlatform::openUrl("https://github.com/aui-framework/aui"); }),
-      _new<AButton>("Visit docs")
-          .connect(&AView::clicked, this, [] { APlatform::openUrl("https://aui-framework.github.io/"); }),
-      _new<AButton>("Submit an issue")
-          .connect(
-              &AView::clicked, this, [] { APlatform::openUrl("https://github.com/aui-framework/aui/issues/new"); }),
-      CustomLayout {} & mUpdater->status.readProjected([&updater = mUpdater](const std::any& status) -> _<AView> {
-          if (std::any_cast<AUpdater::StatusIdle>(&status)) {
-              return _new<AButton>("Check for updates").connect(&AView::clicked, AUI_SLOT(updater)::checkForUpdates);
-          }
-          if (std::any_cast<AUpdater::StatusCheckingForUpdates>(&status)) {
-              return Label { "Checking for updates..." };
-          }
-          if (auto downloading = std::any_cast<AUpdater::StatusDownloading>(&status)) {
-              return Vertical {
-                  Label { "Downloading..." },
-                  _new<AProgressBar>() & downloading->progress,
-              };
-          }
-          if (std::any_cast<AUpdater::StatusWaitingForApplyAndRestart>(&status)) {
-              return _new<AButton>("Apply update and restart")
-                  .connect(&AView::clicked, AUI_SLOT(updater)::applyUpdateAndRestart);
-          }
-          return nullptr;
-      }),
-      Label { "Btw, 2 + 2 = {}"_format(sum(2, 2)) },
-      Label { "Version: " AUI_PP_STRINGIZE(AUI_CMAKE_PROJECT_VERSION) },
-    } });
+MainWindow::MainWindow() : AWindow("Hello World", 300_dp, 200_dp) {
+    setContents(
+        Centered {
+            Vertical {
+                Centered { Label { "Hello world" },
+                }
+            }
+        }
+    );
 }
-
-int MainWindow::sum(int a, int b) { return a + b; }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -1,13 +1,8 @@
 #pragma once
 
 #include <AUI/Platform/AWindow.h>
-#include "MyUpdater.h"
 
 class MainWindow: public AWindow {
 public:
-    MainWindow(_<MyUpdater> updater);
-    static int sum(int a, int b);
-
-private:
-    _<MyUpdater> mUpdater;
+    MainWindow();
 };
