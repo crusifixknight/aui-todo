611d10030c2cc323dc3ae6bd564ca64bf77afc3e
crusifixknight <equinox.salieri@gmail.com>
2025-11-28 21:05:46 +0300

    wip

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,20 +2,32 @@
 #include <AUI/Util/UIBuildingHelpers.h>
 #include <AUI/View/ALabel.h>
 #include <AUI/View/AButton.h>
-#include <AUI/Platform/APlatform.h>
-#include <AUI/View/ADrawableView.h>
-#include <AUI/View/AProgressBar.h>
 #include <AUI/Platform/AWindow.h>
+#include <AUI/View/AHDividerView.h>
+#include <AUI/View/ATextField.h>
+#include <AUI/View/ASpacerFixed.h>
+#include <AUI/View/AListView.h>
 
 using namespace declarative;
 
-MainWindow::MainWindow() : AWindow("Hello World", 300_dp, 200_dp) {
+
+MainWindow::MainWindow() : AWindow("Todo Application", 700_dp, 600_dp) {
     setContents(
-        Centered {
-            Vertical {
-                Centered { Label { "Hello world" },
-                }
-            }
-        }
+        Stacked { 
+          Vertical::Expanding {
+            Centered{
+              Label { "TODO List" } AUI_WITH_STYLE { FontSize { 25_dp } }
+            },
+            _new<ASpacerFixed>(30_dp),
+            _new<AHDividerView>,
+            _new<ASpacerFixed>(15_dp),
+            Horizontal{
+              _new<ATextField>() AUI_WITH_STYLE { Expanding {1} },
+              _new<AButton>("ADD"),
+            },
+            _new<AListView>() ,
+          }
+
+      }
     );
 }
